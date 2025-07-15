19d41d56d4943adb813f5343c4a0033b55d80bbe
Julien LEDENT <jledent@abcoop.be>
2025-07-15 09:50:48 +0200

    Add Slider

--- front/portfolio/src/component/Projets.jsx
+++ front/portfolio/src/component/Projets.jsx
@@ -1,10 +1,24 @@
 import { useState, useEffect, useRef } from "react";
 import styles from "./Projets.module.css";
 import videoSrc from "../assets/Web App AS.mp4";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 function Projets() {
   const [opacity, setOpacity] = useState(1);
   const videoRef = useRef(null);
+
+  //Slider settings
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
 
   //Lancer à vidéo à 6 secondes
   useEffect(() => {
@@ -33,10 +47,26 @@ function Projets() {
   return (
     <div style={{ opacity }}>
       <h3>Projets</h3>
-      <div className={styles.projets1}>
-        <video ref={videoRef} autoPlay muted loop>
-          <source src={videoSrc} type="video/mp4"></source>
-        </video>
+      <div className={styles.projets}>
+        <Slider {...settings}>
+          <div className={styles.project1}>
+            <h4>
+              Web App AS : Application web simple de prise de rendez-vous. La
+              première plage horaire disponible est définie lors de la prise de
+              rendez-vous, avec l'un des assistants sociaux disponibles sur la
+              plateforme.
+            </h4>
+            <video ref={videoRef} autoPlay muted loop>
+              <source src={videoSrc} type="video/mp4"></source>
+            </video>
+          </div>
+          <div className={styles.project2}>
+            <p>Projet en cours...</p>
+          </div>
+          <div className={styles.project3}>
+            <p>Projet en cours...</p>
+          </div>
+        </Slider>
       </div>
     </div>
   );
