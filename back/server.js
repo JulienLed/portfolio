8c567d5b3209dc800d20af7a8f408e323eeec953
Julien LEDENT <jledent@abcoop.be>
2025-07-18 12:03:38 +0200

    Good By .env

--- back/server.js
+++ back/server.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const cors = require("cors");
+const helmet = require("helmet");
+const nodemailer = require("nodemailer");
 
 dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+app.use(cors);
+app.use(helmet);
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
